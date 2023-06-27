7a6ce528778fddfa8fe85ceb43f760844dc870a0
mbugua7890 <meshackmbugua369@gmail.com>
2023-06-27 12:28:23 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#undef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _puts(char *str);
+void prints_rev(char *s);
+void rev_string(char *str);
+void puts2(char *str);
+void puts_half(char *str);
+void prints_arrays(int *a, int n);
+char *_strcpy(char *des, char *src);
+
+#endif
